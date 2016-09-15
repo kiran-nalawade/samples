b35545e2aeacdb6af216de0cff726e904b8a5085
kiran-nalawade <kiran.d.nalawade@gmail.com>
2016-09-16 02:10:37 +0530

    Added question link

--- random/sum_of_digits_from_1_to_N.java
+++ random/sum_of_digits_from_1_to_N.java
@@ -1,15 +1,23 @@
-static int sum_of_digits_1_N(int no){
+class sum_of_digits_from_1_N {
 
-if(no < 10) return (no *(no+1)/2);
+	static int sum_of_digits_1_N(int no){
 
-int digit_places = (int) Math.log10(no);
-int p10 = (int) Math.pow(10, digit_places);
-int first_digit = (int) (no / p10);
+		if(no < 10) return (no *(no+1)/2);
 
-int result = 45* (p10 / 10) * first_digit * digit_places;
+		int digit_places = (int) Math.log10(no);
+		int p10 = (int) Math.pow(10, digit_places);
+		int first_digit = (int) (no / p10);
 
-int remaining = (int) (no % p10);
+		int result = 45* (p10 / 10) * first_digit * digit_places;
 
-return (int) (result + first_digit + first_digit * remaining + 
-(first_digit * (first_digit-1) /2 * p10) + sum_of_digits_1_N(remaining));
+		int remaining = (int) (no % p10);
+
+		return (int) (result + first_digit + first_digit * remaining + 
+					(first_digit * (first_digit-1) /2 * p10) + sum_of_digits_1_N(remaining));
+	}
+	
+	public static void main(String[] args){
+		
+		System.out.println(sum_of_digits_1_N(324));
+	}
 }
